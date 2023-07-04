c94f074533e9aafc0a307cf87d6fa1695c2e26d3
Stella Johansson <stellajoh9@gmail.com>
2023-07-04 18:18:53 +0200

    completed lab

---
+++ index.js
@@ -0,0 +1,15 @@
+let name = "Stella"
+let language = "JavaScript"
+
+function introduction(name) {
+    return(`Hi, my name is ${name}.`)
+}
+
+
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional(name = "Stella", language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
